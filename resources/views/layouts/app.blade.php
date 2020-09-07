d80720ff2f5b43c4878a0a847046b87ec171b70e
Tshego Moloto <tshegofatsosemenya@gmail.com>
2020-09-07 16:00:12 +0200

    Project Name Change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'SwayDev CMS') }}</title>
+    <title>{{ config('app.name', 'SwayDev Business Dashboard') }}</title>
 
     <!-- Scripts -->
     
