5468e1d7f01b6ad35f45c5b9e5f2ae7b1236e1a9
Tshego Moloto <tshegofatsosemenya@gmail.com>
2020-10-06 10:30:51 +0200

    Update live repo

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,14 +3,12 @@
 @section('content')
   
     <div class="row container-fluid">
-        <div class="col-md-2 side-menu" style="background-color:#A4A4AA; height: 100%;">
+        <div class="col-md-2 side-menu" style="background-color:#d4d4d4; height: 100%;">
             @include('dashboard.sideMenu')
         </div>
         <div class="col-md-10 dashboard-content">
-            <div class="content container">
-                    @include('dashboard.index.index')
-            </div>
+            @include('dashboard.index.index')
         </div>
     </div>
-  
+    
 @endsection
